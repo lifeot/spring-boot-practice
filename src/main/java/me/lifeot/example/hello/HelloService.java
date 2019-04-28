2f70a62e3ab6a232e4929c273fdc04005e5a6911
lifeot <lifeot@gmail.com>
2019-04-29 06:44:51 +0900

    Dev Tools & Restart

---
+++ src/main/java/me/lifeot/example/hello/HelloService.java
@@ -0,0 +1,11 @@
+package me.lifeot.example.hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getMessage() {
+        return "Hello Spring Boot 2.0!!!";
+    }
+}
