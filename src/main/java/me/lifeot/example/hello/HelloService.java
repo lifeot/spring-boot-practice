dc06dde717c59ee2186bc92fc9e391d065eb7f14
lifeot <lifeot@gmail.com>
2019-05-02 20:36:39 +0900

    Externalized Configuration

--- src/main/java/me/lifeot/example/hello/HelloService.java
+++ src/main/java/me/lifeot/example/hello/HelloService.java
@@ -1,12 +1,16 @@
 package me.lifeot.example.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
 
+    @Value("${lifeot.name}")
+    String name;
+
     public String getMessage() {
-        return "Hello World";
+        return "Hello, " + name;
     }
 
 //    @PostConstruct
